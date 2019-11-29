19ed2104a252792ceebb61390feb591f1bc879c7
FöldiBence <foldib555@gmail.com>
2019-11-29 12:34:21 +0100

    Add project files.

---
+++ Latvanyossagok/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Latvanyossagok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonkuldes = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lakossagnumeric = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lakossagnumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonkuldes
+            // 
+            this.buttonkuldes.Location = new System.Drawing.Point(145, 23);
+            this.buttonkuldes.Name = "buttonkuldes";
+            this.buttonkuldes.Size = new System.Drawing.Size(75, 23);
+            this.buttonkuldes.TabIndex = 0;
+            this.buttonkuldes.Text = "kuldes";
+            this.buttonkuldes.UseVisualStyleBackColor = true;
+            this.buttonkuldes.Click += new System.EventHandler(this.buttonkuldes_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "adja meg a telepules nevet amit fel akar venni";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "adja meg a telepules letszamat";
+            // 
+            // lakossagnumeric
+            // 
+            this.lakossagnumeric.Location = new System.Drawing.Point(15, 75);
+            this.lakossagnumeric.Name = "lakossagnumeric";
+            this.lakossagnumeric.Size = new System.Drawing.Size(120, 20);
+            this.lakossagnumeric.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "kilepes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 143);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lakossagnumeric);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonkuldes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lakossagnumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonkuldes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown lakossagnumeric;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Latvanyossagok/Form1.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latvanyossagok
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection("Server=localhost;Database=latvanyossagokdb;Uid=root;Pwd=;");
+            conn.Open();
+            var command = conn.CreateCommand();
+            command.CommandText = @"CREATE TABLE `latvanyossagokdb`.`varosok`
+            ( `id` INT NOT NULL AUTO_INCREMENT , `nev` VARCHAR(70) NOT NULL ,
+            PRIMARY KEY (`id`), UNIQUE (`nev`)) ENGINE = InnoDB,
+            ALTER TABLE `varosok` ADD `lakossag` INT NOT NULL AFTER `nev`;
+            ) ";
+            command.CommandText = @"CREATE TABLE `latvanyossagokdb`.`latvanyossagok` ( `id` INT NOT NULL AUTO_INCREMENT ,
+            `nev` VARCHAR(70) NOT NULL ,
+            `leiras` VARCHAR(70) NOT NULL ,  `ar` INT NOT NULL ,
+            `varos_id` INT NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;)";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonkuldes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    var cmd = conn.CreateCommand();
+                    cmd.CommandText = @"INSERT INTO varosok(nev,lakossag) VALUES (@nev, @lakossag)";
+                    cmd.Parameters.AddWithValue("@nev", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@lakossag", lakossagnumeric.Value);
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("nem toltott ki mindent");
+                }
+               
+            }
+            catch (MySqlException ex )
+            {
+                if (ex.Number == 1062)
+                {
+                    MessageBox.Show("mar van ilyen nevu varos");
+                }
+                
+            }
+            
+
+
+
+        }
+    }
+}
